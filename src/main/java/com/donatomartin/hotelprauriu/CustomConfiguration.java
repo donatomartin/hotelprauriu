765f1e5388b86bfa998bde134d995571ed183409
dononitram <uo288787@uniovi.es>
2025-01-08 22:34:02 +0100

    first commit

---
+++ src/main/java/com/donatomartin/hotelprauriu/CustomConfiguration.java
@@ -0,0 +1,34 @@
+package com.donatomartin.hotelprauriu;
+
+import java.util.Locale;
+import org.springframework.lang.NonNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class CustomConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    LocaleResolver localeResolver() {
+        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        localeResolver.setDefaultLocale(Locale.of("es", "ES"));
+        return localeResolver;
+    }
+
+    @Bean
+    LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor =
+                new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    public void addInterceptors(@NonNull InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+}
---
+++ src/test/java/com/donatomartin/hotelprauriu/HotelprauriuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donatomartin.hotelprauriu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelprauriuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
